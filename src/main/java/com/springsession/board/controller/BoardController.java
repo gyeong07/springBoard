2c2dc06b3a626b5c8774785d7b3d421291d43169
SooGyeong <vlem4646@gmail.com>
2022-11-21 23:12:05 +0900

    Create boardmodify

--- src/main/java/com/springsession/board/controller/BoardController.java
+++ src/main/java/com/springsession/board/controller/BoardController.java
@@ -2,9 +2,12 @@ package com.springsession.board.controller;
 
 import com.springsession.board.entity.crud;
 import com.springsession.board.service.BoardService;
+import org.hibernate.query.criteria.internal.expression.function.CurrentDateFunction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -22,5 +25,44 @@ public class BoardController {
     public String boardWritePro(crud crud) {
         boardService.write(crud);
         return "";
+    }
+
+    @GetMapping("/board/list")
+    public String boardList(Model model){
+
+        model.addAttribute("list",boardService.boardList());
+        return "boardlist";
+    }
+
+    @GetMapping("/board/view") //http://localhost:8080/board/view?id=1
+    public String boardView(Model model, Integer id) {
+        model.addAttribute("crud", boardService.boardView(id));
+        return "boardView";
+    }
+
+    @GetMapping("/board/delete")
+    public String boardDelete(Integer id){
+
+        boardService.boardDelete(id);
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("/board/modify/{id}")
+    public String boardModify(@PathVariable("id") Integer id, Model model){
+
+        model.addAttribute("crud",boardService.boardView(id));
+        return "boardmodify";
+    }
+
+    @PostMapping("/board/update/{id}")
+    public String boardUpdate(@PathVariable("id") Integer id, crud crud){
+
+        crud boardTemp = boardService.boardView(id);
+        boardTemp.setTitle(crud.getTitle());
+        boardTemp.setContent(crud.getContent());
+
+        boardService.write(boardTemp);
+
+        return "redirect:/board/list";
     }
 }
--- src/main/java/com/springsession/board/service/BoardService.java
+++ src/main/java/com/springsession/board/service/BoardService.java
@@ -5,13 +5,31 @@ import com.springsession.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BoardService {
 
+    //글 작성
     @Autowired
     private BoardRepository boardRepository;
 
     public void write(crud crud) {
         boardRepository.save(crud);
+    }
+
+    //게시물 리스트 처리
+    public List<crud> boardList() {
+        return boardRepository.findAll();
+    }
+
+    //특정 게시물 불러오기
+    public crud boardView(Integer id) {
+        return boardRepository.findById(id).get();
+    }
+
+    //특정 게시글 삭제
+    public void boardDelete(Integer id){
+        boardRepository.deleteById(id);
     }
 }
